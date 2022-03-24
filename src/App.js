95d464b10ec693ec05f5fa0e3a03d9c2b191e874
nguyenvanhieu1996 <nguyenhieu4477@gmail.com>
2022-03-25 00:00:38 +0700

    update link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import {useEffect} from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from '../src/components/home';
 
 function App() {
+  useEffect(() => {
+    document.title = "Nikko AM"
+  }, [])
   return (
     <BrowserRouter>
       <Routes>
